5d8e347140b065122a84f689f29f11a79cfe4339
ahmedsport <ahmedsport@hotmail.com>
2022-12-18 20:19:09 +0000

    wrking with interfaces

---
+++ src/main/java/oop/polymorphism/Bicycle.java
@@ -0,0 +1,24 @@
+package oop.polymorphism;
+
+public class Bicycle {
+    private int currentSpeed;
+
+    public Bicycle() {
+    }
+
+    public Bicycle(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void move(int amount) {
+        this.currentSpeed += amount;
+    }
+
+    public void applyBreaks (int amount) {
+        this.currentSpeed -= amount;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+}
