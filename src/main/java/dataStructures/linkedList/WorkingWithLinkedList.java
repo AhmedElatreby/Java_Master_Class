48b668f71c1d2c601ab0652c2554e42aaea2892b
ahmedsport <ahmedsport@hotmail.com>
2022-12-22 00:28:59 +0000

    working with linkedlist

--- src/main/java/dataStructures/linkedList/WorkingWithLinkedList.java
+++ src/main/java/dataStructures/linkedList/WorkingWithLinkedList.java
@@ -1,4 +1,33 @@
 package dataStructures.linkedList;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class WorkingWithLinkedList {
+
+    public static void main(String[] args) {
+        LinkedList<Person> people = new LinkedList<>();
+        people.add(new Person("Shahd", 9));
+        people.add(new Person("Jana", 7));
+        people.add(new Person("Sarah", 4));
+        people.add(new Person("Yesmine", 4));
+        System.out.println(people);
+
+        // introduced local variable
+        ListIterator<Person> personListIterator = people.listIterator();
+        // while loop to go through the list
+        while ( personListIterator.hasNext()) {
+            System.out.println(personListIterator.next());
+        }
+        System.out.println();
+
+        // while loop to go through the list in reverse order
+        while ( personListIterator.hasPrevious()) {
+            System.out.println(personListIterator.previous());
+        }
+    }
+
+    public record Person(String name, int age) {
+
+    }
 }
