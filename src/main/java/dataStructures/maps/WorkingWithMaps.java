d14fb20273f31c78f19d494b2779cb7637e9301a
ahmedsport <ahmedsport@hotmail.com>
2022-12-28 12:45:09 +0000

    working with HashMap

--- src/main/java/dataStructures/maps/WorkingWithMaps.java
+++ src/main/java/dataStructures/maps/WorkingWithMaps.java
@@ -1,9 +1,23 @@
 package dataStructures.maps;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 
 public class WorkingWithMaps {
     public static void main(String[] args) {
+        Map<Person, Diamond> map = new HashMap<>();
+        map.put(new Person("Alex"), new Diamond("di"));
+        System.out.println(map.get(new Person("Alex")));
+        System.out.println(map.values());
+        System.out.println(new Person("Ahmed").hashCode());
+        System.out.println(new Person("Ahmed").hashCode());
+
+    }
+
+
+    private static void maps() {
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, new Person("Ahmed"));
         map.put(1, new Person("Ahmed"));// it will ignored
@@ -28,11 +42,25 @@ public class WorkingWithMaps {
 
         map.entrySet().forEach(x -> System.out.println(x.getKey() + " " + x.getValue()));
         System.out.println();
-        map.forEach((key, person)-> {
+        map.forEach((key, person) -> {
             System.out.println(key + " - " + person);
         });
     }
-    record Person(String name){
+
+    static class Person {
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
@@ -46,5 +74,9 @@ public class WorkingWithMaps {
             return Objects.hash(name);
         }
     }
+
+    record Diamond(String name) {
+    }
+
 
 }
