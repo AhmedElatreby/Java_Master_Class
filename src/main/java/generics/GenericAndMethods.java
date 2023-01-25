16b0c1fdee0f258d6f5715b8b0fa9bcb2c901ade
ahmedsport <ahmedsport@hotmail.com>
2023-01-25 13:47:36 +0000

    Generic and methods

---
+++ src/main/java/generics/GenericAndMethods.java
@@ -0,0 +1,22 @@
+package generics;
+
+public class GenericAndMethods {
+    public static void main(String[] args) {
+        String[] names = {"Ahmed", "Shahd"};
+        Character[] letters = {'a', 'b', 'c'};
+        Integer[] numbers = {1,2,3,4,5};
+        print(names);
+        print(letters);
+        print(numbers);
+
+    }
+
+    static <T> void print(T[] array) {
+        for (T e : array) {
+            System.out.println(
+                    e.getClass().getName() +
+                            " - " + e
+            );
+        }
+    }
+}
