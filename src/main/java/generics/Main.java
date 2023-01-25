a806003759f3049aef5999278de4a1e3a555eb70
ahmedsport <ahmedsport@hotmail.com>
2023-01-25 12:46:07 +0000

    Class Cast Exception

---
+++ src/main/java/generics/Main.java
@@ -0,0 +1,16 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        List <Integer>numbers = new ArrayList();
+        numbers.add(1);
+        numbers.add(Integer.valueOf("2"));
+        for (Object number: numbers) {
+            System.out.println( number);
+           }
+    }
+}
