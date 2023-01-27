a31679549b4e2ceb0b473b7786f725ef7cd029eb
ahmedsport <ahmedsport@hotmail.com>
2023-01-27 15:58:09 +0000

    Arrays Conditionals and Methods

--- src/main/java/exercises/ArraysConditionalsAndMethods/Main.java
+++ src/main/java/exercises/ArraysConditionalsAndMethods/Main.java
@@ -2,6 +2,15 @@ package exercises.ArraysConditionalsAndMethods;
 
 public class Main {
     public static void main(String[] args) {
+        String myString = "Hello";
+        reverse(myString);
 
+    }
+     static String reverse(String str) {
+
+        for (int i = str.length() -1; i >= 0; i--) {
+            System.out.print(str.toUpperCase().charAt(i));
+        }
+        return str;
     }
 }
