93d25526ffa1cb51380ccc6d38da632cea317aee
ahmedsport <ahmedsport@hotmail.com>
2023-01-27 17:15:57 +0000

    trim string

--- src/main/java/exercises/ArraysConditionalsAndMethods/Main.java
+++ src/main/java/exercises/ArraysConditionalsAndMethods/Main.java
@@ -3,14 +3,32 @@ package exercises.ArraysConditionalsAndMethods;
 public class Main {
     public static void main(String[] args) {
         String myString = "Hello";
+        String str = "   amig os cod e  ";
+        trimStr(str);
+
         reverse(myString);
 
     }
-     static String reverse(String str) {
 
-        for (int i = str.length() -1; i >= 0; i--) {
+    static String reverse(String str) {
+
+        for (int i = str.length() - 1; i >= 0; i--) {
             System.out.print(str.toUpperCase().charAt(i));
         }
         return str;
     }
+
+    static void trimStr(String str) {
+        String s = "";
+        for (int i = 0; i < str.length(); i++) {
+            s = str.trim().replace(" ", "");
+        }
+        String letter = String.valueOf(s.charAt(0));
+        String word = s.substring(1);
+        letter = letter.toUpperCase();
+        System.out.println(letter + word);
+
+    }
+
+
 }
